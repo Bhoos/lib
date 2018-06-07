49ddc450cbfe1e0ca7e3b5c6e0a02fbd65cea2af
Ranjan Shrestha <ranjan@sharingapples.com>
2018-06-07 12:05:24 +0545

    Provide context for this

--- packages/promisify/src/index.js
+++ packages/promisify/src/index.js
@@ -1,12 +1,12 @@
-export function promisify(fn) {
+export function promisify(fn, context) {
   return (...args) => new Promise((resolve, reject) => {
-    fn(...args, (err, res) => {
+    fn.apply(context || fn, [...args, (err, res) => {
       if (err) {
         return reject(err);
       }
 
       return resolve(res);
-    });
+    }]);
   });
 }
 
@@ -15,7 +15,8 @@ export function promisifyAll(obj) {
   Object.keys(obj).forEach((key) => {
     const fn = obj[key];
     if (typeof fn === 'function') {
-      res[key] = promisify(fn);
+      res[key] = promisify(fn, obj);
     }
   });
+  return res;
 }
