7938d24cdbeb23ef984ee6b5d2c10e0c04d6a5ef
Ranjan Shrestha <ranjan@sharingapples.com>
2018-06-08 10:54:14 +0545

    Allow global and local secondary indexes

--- packages/dynamodb/src/_Key.js
+++ packages/dynamodb/src/_Key.js
@@ -7,20 +7,40 @@ export const HASH = 'HASH';
 export const RANGE = 'RANGE';
 
 class Key extends Field {
-  constructor() {
-    super();
-    this.auto = false;
-    this.keyType = HASH;
+  constructor(name) {
+    super(name);
+    this.isAuto = false; // Does the key needs to be automatically generated
+    this.keyType = HASH;  // The type of the key either hash (partition) or range (sort)
+    this.globalIndexName = null; // Is this key part of a global secondary index
+    this.localIndexName = null; // Is this key part of a local secondary index
   }
 
   sort() {
+    if (this.isAuto) {
+      throw new Error(`Key ${this.name} can't have auto and sort field set`);
+    }
+
     this.keyType = RANGE;
     return this;
   }
 
-  setAuto() {
-    this.auto = true;
+  auto() {
+    if (this.keyType !== HASH) {
+      throw new Error(`Key ${this.name} can't have auto and sort field set`);
+    }
+    this.isAuto = true;
     return this;
+  }
+
+  globalIndex(name) {
+    this.globalIndexName = name;
+  }
+
+  localIndex(name) {
+    if (this.keyType === HASH) {
+      throw new Error(`Key ${this.name} is a hash key can't be used for local secondary index`);
+    }
+    this.localIndexName = name;
   }
 }
 
